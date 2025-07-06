3f04897a682bc5d9b3420295ed8f9905a090bb1d
Miguel Muscat <miguelmuscat93@gmail.com>
2025-07-06 16:02:57 +0200

    harpoon keymaps: engram layout and quick assign

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -22,11 +22,20 @@ return {
             vim.keymap.set("n", "<A-h>", function() harpoon:list():select(1) end)
             vim.keymap.set("n", "<A-t>", function() harpoon:list():select(2) end)
             vim.keymap.set("n", "<A-s>", function() harpoon:list():select(3) end)
-            vim.keymap.set("n", "<A-r>", function() harpoon:list():select(4) end)
-            vim.keymap.set("n", "<A-f>", function() harpoon:list():select(5) end)
-            vim.keymap.set("n", "<A-g>", function() harpoon:list():select(6) end)
-            vim.keymap.set("n", "<A-v>", function() harpoon:list():select(7) end)
-            vim.keymap.set("n", "<A-x>", function() harpoon:list():select(8) end)
+            vim.keymap.set("n", "<A-n>", function() harpoon:list():select(4) end)
+            vim.keymap.set("n", "<A-r>", function() harpoon:list():select(5) end)
+            vim.keymap.set("n", "<A-m>", function() harpoon:list():select(6) end)
+            vim.keymap.set("n", "<A-f>", function() harpoon:list():select(7) end)
+            vim.keymap.set("n", "<A-p>", function() harpoon:list():select(8) end)
+
+            vim.keymap.set("n", "<leader><A-h>", function() harpoon:list():replace_at(1) end)
+            vim.keymap.set("n", "<leader><A-t>", function() harpoon:list():replace_at(2) end)
+            vim.keymap.set("n", "<leader><A-s>", function() harpoon:list():replace_at(3) end)
+            vim.keymap.set("n", "<leader><A-n>", function() harpoon:list():replace_at(4) end)
+            vim.keymap.set("n", "<leader><A-r>", function() harpoon:list():replace_at(5) end)
+            vim.keymap.set("n", "<leader><A-m>", function() harpoon:list():replace_at(6) end)
+            vim.keymap.set("n", "<leader><A-f>", function() harpoon:list():replace_at(7) end)
+            vim.keymap.set("n", "<leader><A-p>", function() harpoon:list():replace_at(8) end)
         end
     }
 }
