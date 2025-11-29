4243b37251372bf1892285b75b2ba53aab40fff7
Miguel Muscat <mail@miguelmuscat.me>
2025-11-29 09:01:29 +0100

    add jsx and tsx formatting

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -8,7 +8,9 @@ return {
                 css = { "prettierd", stop_after_first = true },
                 html = { "prettierd" },
                 javascript = { "prettierd", stop_after_first = true },
+                javascriptreact = { "prettierd", stop_after_first = true },
                 typescript = { "prettierd", stop_after_first = true },
+                typescriptreact = { "prettierd", stop_after_first = true },
             },
             default_format_opts = {
                 lsp_format = "fallback",
