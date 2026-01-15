b4157d1466a8449160c69faa698af7451dad9b63
Miguel Muscat <mail@miguelmuscat.me>
2026-01-15 14:42:06 +0100

    update conform config

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -1,23 +1,28 @@
 return {
     {
-        'stevearc/conform.nvim',
+        "stevearc/conform.nvim",
         opts = {
             formatters_by_ft = {
                 lua = { "stylua" },
                 go = { "goimports", "gofmt" },
-                css = { "prettierd", stop_after_first = true },
+                css = { "prettierd" },
                 html = { "prettierd" },
-                javascript = { "prettierd", stop_after_first = true },
-                javascriptreact = { "prettierd", stop_after_first = true },
-                typescript = { "prettierd", stop_after_first = true },
-                typescriptreact = { "prettierd", stop_after_first = true },
+                javascript = { "prettierd" },
+                javascriptreact = { "prettierd" },
+                typescript = { "prettierd" },
+                typescriptreact = { "prettierd" },
             },
             default_format_opts = {
                 lsp_format = "fallback",
             },
         },
         keys = {
-            { "<F3>", function() require("conform").format() end },
+            {
+                "<F3>",
+                function()
+                    require("conform").format({ async = true })
+                end,
+            },
         },
-    }
+    },
 }
