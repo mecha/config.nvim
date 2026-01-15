22bd1ec4716a14567bf5990b76658f1fffbc102a
Miguel Muscat <mail@miguelmuscat.me>
2026-01-15 14:40:36 +0100

    update deprecated leap config

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -3,10 +3,12 @@ return {
         "ggandor/leap.nvim",
         config = function()
             local leap = require("leap")
-            leap.add_default_mappings(true)
             leap.opts = {
                 highlight_unlabeled_phase_one_targets = true,
             }
-        end
+            vim.keymap.set({ "n", "x", "o" }, "s", "<Plug>(leap-forward)")
+            vim.keymap.set({ "n", "x", "o" }, "S", "<Plug>(leap-backward)")
+            vim.keymap.set("n", "gs", "<Plug>(leap-from-window)")
+        end,
     },
 }
